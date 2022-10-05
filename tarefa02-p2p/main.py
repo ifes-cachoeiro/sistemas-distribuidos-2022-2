3bf44a00bc9a1a78d2901138b6bd41e21162f16d
rafaelsilvag <rafaelsg@gmail.com>
2022-10-05 10:09:16 -0300

    adicionar exemplo inicial da tarefa 02

--- tarefa02-p2p/main.py
+++ tarefa02-p2p/main.py
@@ -1,3 +1,38 @@
 import socket
 import _thread
 import json
+
+PORT = 12345
+
+
+class Node:
+    def __init__(self, ip):
+        self.ip = ip
+        self.id = hash(f"{ip}+rafael")
+        self.porta = None
+        self.sucessor = None
+        self.antecessor = None
+
+
+def servidor_p2p(udp, node):
+    print(f"Starting P2P Server on port {PORT}")
+    orig = ("", PORT)
+    udp.bind(orig)
+    while True:
+        msg, cliente = udp.recvfrom(1024)
+
+
+def interface(node):
+    while True:
+        pass
+
+
+def main():
+    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    node = Node()
+    _thread.start_new_thread(servidor_p2p, (udp, node))
+    interface(node)
+
+
+if __name__ == "__main__":
+    main()
