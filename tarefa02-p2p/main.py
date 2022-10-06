3078cf6b0bedc69870ba0a7a3a33d3ee61854e02
rafaelsilvag <rafaelsg@gmail.com>
2022-10-05 21:41:43 -0300

    adiciona versao inicial o main.py

--- tarefa02-p2p/main.py
+++ tarefa02-p2p/main.py
@@ -1,22 +1,23 @@
+#!/usr/sbin/python3
+# -*- coding: utf-8 -*-
 import socket
 import _thread
 import json
-
-PORT = 12345
+import sys
 
 
 class Node:
     def __init__(self, ip):
         self.ip = ip
         self.id = hash(f"{ip}+rafael")
-        self.porta = None
+        self.porta = 12345
         self.sucessor = None
         self.antecessor = None
 
 
 def servidor_p2p(udp, node):
-    print(f"Starting P2P Server on port {PORT}")
-    orig = ("", PORT)
+    print(f"=> Iniciando servidor P2P (ip={node.ip}, porta={node.porta})")
+    orig = ("", node.porta)
     udp.bind(orig)
     while True:
         msg, cliente = udp.recvfrom(1024)
@@ -29,10 +30,14 @@ def interface(node):
 
 def main():
     udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    node = Node()
+    node = Node(ip=sys.argv[1])
     _thread.start_new_thread(servidor_p2p, (udp, node))
     interface(node)
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) == 2:
+        main()
+    else:
+        print("Modo de utilização: python3 main.py <ENDEREÇO_IP>")
+        sys.exit(0)
